b6a9f30f046ae2ec387d5a49e7f1d2e81f334b6b
patriciavandegraaf <86768083+patriciavandegraaf@users.noreply.github.com>
2021-08-31 13:23:36 +0200

    Add files via upload

---
+++ opdracht 11 1ste JS/script.js
@@ -0,0 +1,31 @@
+console.log("Hello Winc Academy")
+let name = "Patricia";
+console.log("Patricia")
+let number = "4+4";
+console.log(4 + 4)
+
+let x = 4;
+let y = 6;
+let z = x - y;
+console.log(y - x)
+
+let a = 4;
+let b = 12;
+let c = a * b;
+console.log(a * b)
+
+let d = 4;
+let e = 1;
+let f = d / e;
+console.log(d / e)
+
+let g = 6;
+let h = 112;
+let i = g % h;
+console.log(g % h)
+
+let age = 38;
+console.log(age)
+
+let text = "achtendertig";
+console.log(text)
