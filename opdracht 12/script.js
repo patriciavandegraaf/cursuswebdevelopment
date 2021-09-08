3d6b9bf4dd926d5de4731d204ee0d01601adf2c1
patriciavandegraaf <86768083+patriciavandegraaf@users.noreply.github.com>
2021-09-08 19:18:06 +0200

    Add files via upload

---
+++ opdracht 12/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+// dit is een grote som
+/* comment */ 
+let multiplication =1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division =637263 / 54; 
+console.log("division:", division);
