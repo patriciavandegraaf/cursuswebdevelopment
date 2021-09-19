c1d9a871a11300e6910f7395d23f2ee7e3f3bcb0
patriciavandegraaf <86768083+patriciavandegraaf@users.noreply.github.com>
2021-09-19 17:27:17 +0200

    Add files via upload

---
+++ opdracht13 if/script.js
@@ -0,0 +1,36 @@
+const age = 18;
+
+
+if (age >= 18) {
+    console.log("Je bent ouder dan 18 jaar");
+
+}
+else if (age < 18) {
+    console.log("Je bent jonger dan 18 jaar");
+
+}
+
+
+const isFemale = Boolean;
+
+if (boolean = true) {
+    console.log("female");
+}
+else if (boolean = false) {
+    console.log("male");
+}
+
+bob = "bob"
+
+const driverStatus = bob
+
+
+if (diverStatus = bob) {
+    console.log("mag rijden");
+}
+else if (driverStatus != bob) {
+    console.log("niet rijden");
+}
+
+
+
