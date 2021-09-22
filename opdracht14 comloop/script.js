e674682f038301091ea89f926d58c80484a3c342
patriciavandegraaf <86768083+patriciavandegraaf@users.noreply.github.com>
2021-09-22 19:57:23 +0200

    Add files via upload

---
+++ opdracht14 comloop/script.js
@@ -0,0 +1,47 @@
+const age = 18;
+if (age >= 18) {
+    console.log("Je bent ouder dan 18 jaar");
+}
+else {
+    console.log("Je bent jonger dan 18 jaar");
+}
+
+
+const isFemale = Boolean;
+if (boolean = true) {
+    console.log("female");
+}
+else{
+    console.log("male");
+}
+
+bob = "bob"
+const driverStatus = bob
+if (diverStatus = bob) {
+    console.log("mag rijden");
+}
+else if (driverStatus != bob) {
+    console.log("niet rijden");
+}
+
+if (age <=18 && age >=25) {
+    console.log("Je krijgt 50% korting!")
+}else{
+    console.log("je krijgt geen korting")
+}
+const firstName ="sarah";
+if (firtsName = "sarah"){
+    console.log("Je krijgt een gratis biertje");
+}else{
+    console.log("Je moet betalen")
+}
+
+const totalAmount =175;
+if( totalAmount>=25|| totalAmount >=50 || totalAmount>=100){
+    console.log("gratis vega bitterballen");
+}else if (totalAmount>=50){
+    console.log("gratis nachos");
+}else (totalAmount>=100) 
+    console.log("gratis fles champagne");
+
+
