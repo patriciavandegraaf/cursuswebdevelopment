5e847b265e5e3964f7da1a3f64738c9d9799b6f4
patriciavandegraaf <86768083+patriciavandegraaf@users.noreply.github.com>
2021-11-01 10:48:34 +0100

    Add files via upload

---
+++ opdracht15 objectsenarrays/script.js
@@ -0,0 +1,25 @@
+let person= {
+    name: "Patricia",
+    age: 38
+};
+person.name= "Patricia";
+
+console.log(person.name);
+
+
+let evaluations= 
+     [7,10,9]
+
+console.log(evaluations);
+
+let selectedColors= ["rood","groen","blauw"];
+selectedColors[3]="geel"
+selectedColors[4]=5
+selectedColors[5]="hoi, ik ben een object"
+console.log(selectedColors);
+
+let animal={
+    name: "American Bobtail",
+}
+
+console.log("Herkomstkat 1:", catBreeds[0].origin)
